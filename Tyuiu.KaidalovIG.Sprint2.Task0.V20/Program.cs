c2fb55100881767354d743466510ca468d31d60b
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-10-20 16:08:37 +0500

    Add project files.

---
+++ Tyuiu.KaidalovIG.Sprint2.Task0.V20/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KaidalovIG.Sprint2.Task0.V20.Lib;
+
+
+namespace Tyuiu.KaidalovIG.Sprint2.Task0.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            int x = 1075, y = 275;
+            bool[] res = new bool[6];
+            res = dataService.GetCompareOperations(x, y);
+
+            Console.Title = "Спринт #2 | Выполнил: Кайдалов И. Г. | СмартБ 23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Кайдалов Илья Григорьевич | СмартБ 23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений                                *");
+            Console.WriteLine("* (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться)*");
+            Console.WriteLine("* и арифметических выражений,                                             *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив):                   *");
+            Console.WriteLine("* ((True, False, True, False, True, False), при x = 1075, y = 275         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"X = {x}");
+            Console.WriteLine($"Y = {y}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
