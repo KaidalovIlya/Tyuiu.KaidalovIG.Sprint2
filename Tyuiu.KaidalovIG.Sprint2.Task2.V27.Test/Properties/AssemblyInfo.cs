1148734aba485ec31f59c0fb441d96131565a5df
Tyuiu.KaidalovIG@outlook.com <Kaydalov.2026@mail.ru>
2023-10-20 16:35:33 +0500

    Task2

---
+++ Tyuiu.KaidalovIG.Sprint2.Task2.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KaidalovIG.Sprint2.Task2.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KaidalovIG.Sprint2.Task2.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5efe3fc6-262b-4a2b-bd77-b66199352523")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
